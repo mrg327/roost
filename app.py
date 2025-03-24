03086197737ded92efab3feced7140326e5440a7
mrg327 <matthewghere327@gmail.com>
2025-03-23 19:02:53 -0700

    main

--- app.py
+++ app.py
@@ -1,6 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+TITLE = "Roost App"
+
+TEST_TASK_ITEMS = {
+    "task1": {
+        "sub1": "",
+        "sub2": "",
+        "sub3": "",
+    },
+    "task2": {
+        "sub1": "",
+        "sub2": "",
+    },
+    "task3": {
+        "sub1": "",
+    },
+    "task4": {
+        "sub1": "",
+    },
+    "task5": {
+        "sub1": "",
+    },
+}
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", title=TITLE, task_items=TEST_TASK_ITEMS)
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", debug=True)
--- funcs/nest.py
+++ funcs/nest.py
@@ -4,5 +4,18 @@ from dataclasses import dataclass
 
 @dataclass
 class Nest:
+    # id should be unique such that any nest can
+    # be referenced by it
+    id: int
+    # Name of the task that we are trying to complete
     name: str
-    eggs: list[Nest]
+    done: bool
+
+    parent: int
+    children: list
+
+    def add_child(self, id: int):
+        self.children += [id]
+
+    def remove_child(self, id: int):
+        self.children.remove(id)
